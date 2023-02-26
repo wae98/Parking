2051a912bce579a90a39de978dcc7442bb0a5097
WALTER BAMAC <65936945+wae98@users.noreply.github.com>
2023-02-26 16:58:42 -0600

    first commit

--- api/routes/api.php
+++ api/routes/api.php
@@ -22,9 +22,6 @@ Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
     Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::resource('vehiclesTypes', VechicleTypeContoller::class);
+    Route::resource('stays', StaysContoller::class);
 });
-
-
-
-Route::resource('vehiclesTypes', VechicleTypeContoller::class);
-Route::resource('stays', StaysContoller::class);
